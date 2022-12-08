7c12a805c9833e9afb14826a7652e3ddb8bdfb90
Beattris <beatriceebirim@gmail.com>
2022-12-08 23:42:59 +0100

    added a data file for the meme images

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,13 +1,13 @@
 export const Meme = () => {
   return (
     <main>
-      <form className="meme-form">
+      <div className="meme-form">
         <div className="meme-inputs-wrapper">
           <input type="text" placeholder="Top text" />
           <input type="text" placeholder="Bottom text" />
         </div>
         <button className="meme-button">Get a new meme image 🖼</button>
-      </form>
+      </div>
     </main>
   );
 };

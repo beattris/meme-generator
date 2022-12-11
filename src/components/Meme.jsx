c2ede21fe663639908028c6a7f45cbd436ad0c85
Beattris <beatriceebirim@gmail.com>
2022-12-11 22:38:52 +0100

    fetch the meme image from the data file

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,4 +1,15 @@
+import { useState } from "react";
+import memesData from "../memesData";
+
 export const Meme = () => {
+  const [memeImage, setMemeImage] = useState("");
+
+  function getMemeImage(){
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage (memesArray[randomNumber].url);
+  }
+
   return (
     <main>
       <div className="meme-form">
@@ -6,8 +17,9 @@ export const Meme = () => {
           <input type="text" placeholder="Top text" />
           <input type="text" placeholder="Bottom text" />
         </div>
-        <button className="meme-button">Get a new meme image 🖼</button>
-      </div>
+        <button className="meme-button" onClick={getMemeImage}>Get a new meme image 🖼</button>
+        </div>
+        <img src={memeImage} alt="" />
     </main>
   );
 };
